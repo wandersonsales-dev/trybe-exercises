4820393ae912052a487a7af31b7c7f80740d5323
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-02-24 18:40:57 -0300

    [Q02] - Número maior

--- bloco_4/dia_1/ex01.js
+++ bloco_4/dia_1/ex01.js
@@ -1,6 +1,5 @@
 let a = 5;
 let b = 10;
-let resultado;
 
 // ADIÇÃO
 let soma = a + b;
---
+++ bloco_4/dia_1/ex02.js
@@ -0,0 +1,10 @@
+let a = 5;
+let b = 10;
+
+if(a > b) {
+  console.log("A é maior que B");
+} else if(b > a) {
+  console.log("B é maior que A");
+} else {
+  console.log("Os dois números são iguais");
+}
