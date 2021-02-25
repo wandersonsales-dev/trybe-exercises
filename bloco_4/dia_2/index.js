d847632adc7964ba20853765138e8a415688a8c4
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-02-25 15:36:29 -0300

    [Q02] - Soma de todos os elementos do array

--- bloco_4/dia_2/index.js
+++ bloco_4/dia_2/index.js
@@ -6,3 +6,14 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for(let i = 0; i < numbers.length; i++){
   console.log(numbers[i]);
 }
+
+// 2 -  Para o segundo exercício, você deve somar todos os valores contidos no
+//      array e imprimir o resultado;
+
+let acumulador = 0;
+
+for(let i = 0; i < numbers.length; i++){
+  acumulador += numbers[i];
+}
+
+console.log('A soma de todos os elementos é: ' + acumulador);
