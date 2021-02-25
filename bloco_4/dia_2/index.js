45a7a61507c25d1fe09532c58d226afd2eaf0d0d
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-02-25 15:32:23 -0300

    [Q01] - Imprimir todos os elementos do array

--- bloco_4/dia_2/index.js
+++ bloco_4/dia_2/index.js
@@ -1,1 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// 1 -  Nesse primeiro exercício, percorra o array imprimindo todos os
+//      valores nele contidos com a função console.log() ;
+
+for(let i = 0; i < numbers.length; i++){
+  console.log(numbers[i]);
+}
