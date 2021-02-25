c7f44580cb52bfac7d0674c6815c0a801b8caf61
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-02-25 15:53:27 -0300

    [Q06] - Qtd de ímpares

--- bloco_4/dia_2/index.js
+++ bloco_4/dia_2/index.js
@@ -66,3 +66,21 @@ for(let i = 0; i < numbers.length; i++){
 }
 
 console.log('O maior valor do array é: ' + maior);
+
+// 6 -  Descubra quantos valores ímpares existem no array e imprima
+//      o resultado. Caso não exista nenhum, imprima a
+//      mensagem: "nenhum valor ímpar encontrado";
+
+let impares = 0;
+
+for(let i = 0; i < numbers.length; i++){
+  if(numbers[i] % 2 !== 0){
+    impares++;
+  }
+}
+
+if(impares === 0){
+  console.log('nenhum valor ímpar encontrado');
+} else {
+  console.log('Existem ' + impares + ' valores ímpares no array');
+}
