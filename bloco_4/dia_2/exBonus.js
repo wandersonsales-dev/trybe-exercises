520114f1c311d48fada9e6991da3fc3dd39560aa
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-02-25 21:07:48 -0300

    [BÔNUS][Q02] - Ordenar o vetor numbers em ordem decrescente

--- bloco_4/dia_2/exBonus.js
+++ bloco_4/dia_2/exBonus.js
@@ -13,3 +13,17 @@ for(let index = 0; index < numbers.length; index += 1){
 }
 
 console.log(numbers);
+
+// 2 -  Ordene o array numbers em ordem decrescente e imprima seus valores;
+
+for(let index = 0; index < numbers.length; index += 1){
+  for(let seletor = 0; seletor < numbers.length-index; seletor += 1){
+    if(numbers[seletor] < numbers[seletor+1]){
+      let aux = numbers[seletor];
+      numbers[seletor] = numbers[seletor+1];
+      numbers[seletor+1] = aux;
+    }
+  }
+}
+
+console.log(numbers);
