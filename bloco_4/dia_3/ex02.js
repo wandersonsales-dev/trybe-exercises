981879870c1ff6f272b8808587f4789164f9c8ac
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-02-26 19:16:29 -0300

    [Q02] - Triângulo de '*' da esquerda pra direita

---
+++ bloco_4/dia_3/ex02.js
@@ -0,0 +1,19 @@
+// 2-   Para o segundo exercício, faça o mesmo que antes, mas
+//      que imprima um triângulo retângulo com 5 asteriscos de base.
+//      Por exemplo:
+
+//      n = 5
+
+//      *
+//      **
+//      ***
+//      ****
+//      *****
+
+let n = 3;
+let lineTriangle = '';
+
+for(let line = 0; line < n; line += 1){
+  lineTriangle += ' *';
+  console.log(lineTriangle);
+}
