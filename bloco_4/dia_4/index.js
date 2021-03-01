8db545c29764f686b7ec8db2adcc3f0074e01ab4
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-03-01 15:36:26 -0300

    [Q05] - Mostra os valores dos 2 objetos mesclados

--- bloco_4/dia_4/index.js
+++ bloco_4/dia_4/index.js
@@ -50,3 +50,28 @@ for(let key in info){
 for(let key in info){
   console.log(info[key]);
 }
+
+// 5 -  Agora, defina um segundo objeto com a mesma
+//      estrutura (as mesmas chaves) do primeiro e os seguintes
+//      valores: "Tio Patinhas", "Christmas on Bear Mountain, Dell's Four ColorComics #178", "O último MacPatinhas", "Sim".
+//      Valor esperado no console:
+
+//        Margarida e Tio Patinhas
+//        Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
+//        Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
+//        Ambos recorrentes // Atenção para essa última linha!
+
+let outro = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four ColorComics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim",
+}
+
+console.log(info.personagem + " e " + outro.personagem);
+console.log(info.origem + " e " + outro.origem);
+console.log(info.nota + " e " + outro.nota);
+
+if(info.recorrente === 'Sim' && outro.recorrente === 'Sim'){
+  console.log("Ambos recorrentes");
+}
