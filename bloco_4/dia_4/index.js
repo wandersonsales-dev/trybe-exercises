924a26a687f1a64c071c9746d6698a8af6cfb815
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-03-01 15:20:50 -0300

    [Q02] - Inserir 'recorrente' no objeto 'info'

--- bloco_4/dia_4/index.js
+++ bloco_4/dia_4/index.js
@@ -12,3 +12,17 @@ let info = {
 //      Valor esperado no console: Bem-vinda, Margarida
 
 console.log('Bem-vinda, ' + info.personagem);
+
+// 2 -  Insira no objeto uma nova propriedade com o nome de chave "recorrente" e o valor "Sim" e, em seguida, imprima o objeto no console.
+//      Valor esperado no console:
+
+//        {
+//          personagem: 'Margarida',
+//          origem: 'Pato Donald',
+//          nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//          recorrente: 'Sim'
+//        };
+
+info["recorrente"] = 'Sim';
+
+console.log(info);
