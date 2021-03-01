ae7f4a1b60b6124c9bf9f14659623cf39b39013d
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-03-01 15:13:20 -0300

    Estrutura inicial dos exercícios do Bloco 4.4

---
+++ bloco_4/dia_4/index.js
@@ -0,0 +1,8 @@
+// Usando o objeto abaixo, faça os exercícios a seguir:
+
+// let info = {
+//   personagem: "Margarida",
+//   origem: "Pato Donald",
+//   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+// };
+
