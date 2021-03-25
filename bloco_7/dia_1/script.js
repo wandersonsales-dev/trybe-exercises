ba53cc12fba910ffc3e012b1390a3413e1a0ee4f
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-03-25 11:56:20 -0300

    :rocket: [Q01] - Resolução da Q01

--- bloco_7/dia_1/script.js
+++ bloco_7/dia_1/script.js
@@ -1,6 +1,6 @@
-function testingScope(escopo) {
+const testingScope = (escopo) => {
   if (escopo === true) {
-    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
     ifScope += ' ótimo, fui utilizada no escopo !';
     console.log(ifScope);
   } else {
@@ -8,6 +8,6 @@ function testingScope(escopo) {
     console.log(elseScope);
   }
   console.log(`${ifScope} o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
-}
+};
 
 testingScope(true);
