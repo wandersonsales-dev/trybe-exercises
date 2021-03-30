4c73971820fcd806198290334666ee2fab441778
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-03-29 22:56:44 -0300

    :rocket: [PARTE II] - Resolução da Q04

--- bloco_8/dia_1/bonus/bonus.js
+++ bloco_8/dia_1/bonus/bonus.js
@@ -28,13 +28,14 @@ const battleMembers = { mage, warrior, dragon };
 
 // Math.random() * (max - min) + min;
 const damageDragon = () => (Math.random() * (dragon.strength - 15) + 15).toFixed(2);
+battleMembers.dragon.damage = damageDragon;
 
 // Q02. Crie uma função que reotorna o dano causado pelo warrior .
 //      O dano será um número aleatório entre o valor do atributo
 //      strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo).
 
 const damageWarrior = () => (Math.random() * ((warrior.strength * warrior.weaponDmg) - warrior.strength) + warrior.strength).toFixed(2);
-battleMembers.warrior.damage = damageDragon;
+battleMembers.warrior.damage = damageWarrior;
 
 // Q03. Crie uma função que retorna um objeto com duas chaves e dois valores
 //      contendo o dano e a mana gasta pelo mago em um turno.
@@ -46,9 +47,6 @@ battleMembers.warrior.damage = damageDragon;
 //      o valor de dano recebe uma mensagem
 //      (Ex: "Não possui mana suficiente") e a mana gasta é 0.
 
-const attackMageCost = () => (Math.random() * ((mage.intelligence * 2) - mage.intelligence) + mage.intelligence).toFixed(2);
-battleMembers.mage.damage = attackMageCost;
-
 const manaMage = () => {
   if (mage.mana > 15) {
     mage.mana -= 15;
@@ -58,22 +56,27 @@ const manaMage = () => {
 };
 battleMembers.mage.mana = manaMage;
 
+const attackMageCost = () => (Math.random() * ((mage.intelligence * 2) - mage.intelligence) + mage.intelligence).toFixed(2);
+battleMembers.mage.damage = attackMageCost;
+
 // PARTE 02
 
 const gameActions = {
   knightShift: (action) => {
-    battleMembers.dragon.healthPoints -= action();
-    return battleMembers.dragon.healthPoints;
+    battleMembers.dragon.healthPoints -= parseFloat(action());
   },
   mageShift: (action) => {
-    battleMembers.dragon.healthPoints -= action();
+    battleMembers.dragon.healthPoints -= parseFloat(action());
+    battleMembers.mage.mana();
   },
   dragonShift: (action) => {
-    battleMembers.mage.healthPoints -= action();
-    battleMembers.warrior.healthPoints -= action();
+    battleMembers.mage.healthPoints -= parseFloat(action());
+    battleMembers.warrior.healthPoints -= parseFloat(action());
   },
+  getBattleMembers: () => console.log(battleMembers),
 };
 
 gameActions.knightShift(battleMembers.warrior.damage);
 gameActions.mageShift(battleMembers.mage.damage);
 gameActions.dragonShift(battleMembers.dragon.damage);
+gameActions.getBattleMembers();
