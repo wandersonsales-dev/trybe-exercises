614480de10e6f6756e9269970dbd049c2106ab70
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-04-19 19:22:32 -0300

    :rocket: [PARTE I] - Ex02 -> item 04

--- bloco_10/dia_1/__tests__/parte_1/ex02.spec.js
+++ bloco_10/dia_1/__tests__/parte_1/ex02.spec.js
@@ -9,7 +9,11 @@ describe('Questão 2', () => {
     expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
   });
 
-  test('myRemove([1, 2, 3, 4], 3) NÃO deve retornar [1, 2, 3, 4]', () => {
-    expect(myRemove([1, 2, 3, 4], 3)).not.toContainEqual([1, 2, 3, 4]);
+  test('O array [1, 2, 3, 4] NÃO deve sofrer alterações', () => {
+    expect(myRemove([1, 2, 3, 4])).toEqual([1, 2, 3, 4]);
+  });
+
+  test('myRemove([1, 2, 3, 4], 5) deve retornar [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).not.toContain(5);
   });
 });
