02fcbbb734ca4ce1e2db072e64edc8a7c4f97208
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-04-19 18:45:50 -0300

    :rocket: [PARTE I] - Ex01 -> item 02

--- bloco_10/dia_1/__tests__/parte_1/ex01.spec.js
+++ bloco_10/dia_1/__tests__/parte_1/ex01.spec.js
@@ -4,4 +4,8 @@ describe('Questão 01', () => {
   test('A soma de 4 e 5 é igual a 9', () => {
     expect(sum(4, 5)).toBe(9);
   });
+
+  test('A soma de 0 e 0 é igual a 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
 });
