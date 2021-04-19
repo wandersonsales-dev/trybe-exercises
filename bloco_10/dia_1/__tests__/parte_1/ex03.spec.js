29b9bbdb209c3adad502980884d1724b320920af
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-04-19 19:29:40 -0300

    :rocket: [PARTE I] - Ex03 -> item 01

---
+++ bloco_10/dia_1/__tests__/parte_1/ex03.spec.js
@@ -0,0 +1,8 @@
+const myRemoveWithoutCopy = require('../../parte_1/ex03');
+
+describe('Questão 3', () => {
+  test('myRemoveWithoutCopy([1, 2, 3, 4], 3) deve retornar [3]', () => {
+    const arr = [1, 2, 3, 4];
+    expect(myRemoveWithoutCopy(arr, 3)).toEqual([1, 2, 4]);
+  });
+});
