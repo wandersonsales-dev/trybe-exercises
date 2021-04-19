a30b6c698aa41a8d21a3d265cd8781332186d2cd
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-04-19 19:11:59 -0300

    :rocket: [PARTE I] - Ex02 -> item 02

--- bloco_10/dia_1/__tests__/parte_1/ex02.spec.js
+++ bloco_10/dia_1/__tests__/parte_1/ex02.spec.js
@@ -4,4 +4,8 @@ describe('Questão 2', () => {
   test('myRemove([1, 2, 3, 4], 3) deve retornar [1, 2, 4]', () => {
     expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
   });
+
+  test('myRemove([1, 2, 3, 4], 3) NÃO deve retornar [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
 });
