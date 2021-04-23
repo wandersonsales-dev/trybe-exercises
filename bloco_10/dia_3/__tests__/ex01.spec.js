aabb72f74800320f69166967f55a2851e2585cbd
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-04-22 22:18:01 -0300

    :rocket: Questão 01

---
+++ bloco_10/dia_3/__tests__/ex01.spec.js
@@ -0,0 +1,12 @@
+let generateRandomNumber = require('../ex01');
+
+describe('Questão 01', () => {
+  it('Testa se função retorna um valor 10.', () => {
+    generateRandomNumber = jest.fn().mockReturnValue(10);
+
+    generateRandomNumber();
+    expect(generateRandomNumber).toHaveBeenCalled();
+    expect(generateRandomNumber()).toBe(10);
+    expect(generateRandomNumber).toHaveBeenCalledTimes(2);
+  });
+});
---
+++ bloco_10/dia_3/ex01.js
@@ -0,0 +1,8 @@
+// Q01. Crie uma função que gere um número aleatório entre 0 e 100.
+//      Você irá criar também os testes para essa função. Utilizando
+//      o mock, defina o retorno padrão como 10. Teste se a função
+//      foi chamada, qual seu retorno e quantas vezes foi chamada.
+
+const generateRandomNumber = () => Math.round(Math.random() * 100);
+
+module.exports = generateRandomNumber;
