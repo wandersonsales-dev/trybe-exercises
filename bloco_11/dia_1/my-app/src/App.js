3bfda5ac06b7ec6324cd3159d07f6c78a1ac0ea5
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-04-26 20:26:55 -0300

    :rocket: Questão 02 -> 1

--- bloco_11/dia_1/my-app/src/App.js
+++ bloco_11/dia_1/my-app/src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+const task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
 function App() {
   return (
     <div className="App">
