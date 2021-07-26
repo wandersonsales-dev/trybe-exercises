0b7142d71dfa75308dd2d20a00c884b9e0cbe134
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-07-25 22:35:27 -0300

    :sparkles: Exercise 03
    3 - Estamos com uma média de 39.494 de vendas de camisas por mês. Qual é o valor aproximado para baixo dessa média?

--- bloco_21/dia_1/funcoes_matematicas.sql
+++ bloco_21/dia_1/funcoes_matematicas.sql
@@ -15,3 +15,6 @@ SELECT
 -- 2 - Monte uma query que exiba o valor arredondado de 15.7515971 com uma precisão de 5 casas decimais.
 SELECT
   ROUND(15.7515971, 5);
+-- 3 - Estamos com uma média de 39.494 de vendas de camisas por mês. Qual é o valor aproximado para baixo dessa média?
+SELECT
+  FLOOR(39.494);
