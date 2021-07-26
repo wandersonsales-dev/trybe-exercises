4346540d562fc451ba9c0093340d177980ed80fe
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-07-26 20:59:54 -0300

    :sparkles: Exercise 02
    2 - Monte uma query para a tabela sakila.customer que exiba a quantidade de clientes ativos e inativos por loja. Os resultados devem conter o ID da loja , o status dos clientes (ativos ou inativos) e a quantidade de clientes por status.

--- bloco_21/dia_1/groupby.sql
+++ bloco_21/dia_1/groupby.sql
@@ -6,3 +6,13 @@ from
   sakila.customer
 GROUP BY
   active;
+-- 2 - Monte uma query para a tabela sakila.customer que exiba a quantidade de clientes ativos e inativos por loja. Os resultados devem conter o ID da loja , o status dos clientes (ativos ou inativos) e a quantidade de clientes por status.
+SELECT
+  store_id,
+  active,
+  COUNT(*)
+FROM
+  sakila.customer
+GROUP BY
+  store_id,
+  active;
