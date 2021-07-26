76ba20ca1062c8fefc55de6ff9c3ca8a2d878a01
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-07-25 21:46:51 -0300

    :sparkles: Exercise 02
    2 - Faça uma query que transforme a frase 'Você já ouviu falar do DuckDuckGo?' em 'Você já ouviu falar do Google?'.

--- bloco_21/dia_1/exercises_manipulacao_de_strings.sql
+++ bloco_21/dia_1/exercises_manipulacao_de_strings.sql
@@ -1,3 +1,10 @@
 -- 1 - Faça uma query que exiba a palavra 'trybe' em CAIXA ALTA.
 SELECT
   UCASE('trybe');
+-- 2 - Faça uma query que transforme a frase 'Você já ouviu falar do DuckDuckGo?' em 'Você já ouviu falar do Google?' .
+SELECT
+  REPLACE(
+    'Você já ouviu falar do DuckDuckGo?',
+    'DuckDuckGo',
+    'Google'
+  );
