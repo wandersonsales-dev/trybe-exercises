f47a511b68f50ef0fdc6e74887adf16e8c6fa745
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-07-25 21:54:26 -0300

    :sparkles: Exercise 05
    5 - Por fim, padronize a string 'RUA NORTE 1500, SÃO PAULO, BRASIL' para que suas informações estejam todas em caixa baixa.

--- bloco_21/dia_1/exercises_manipulacao_de_strings.sql
+++ bloco_21/dia_1/exercises_manipulacao_de_strings.sql
@@ -18,3 +18,6 @@ SELECT
     12,
     11
   );
+-- 5 - Por fim, padronize a string 'RUA NORTE 1500, SÃO PAULO, BRASIL' para que suas informações estejam todas em caixa baixa.
+SELECT
+  LCASE('RUA NORTE 1500, SÃO PAULO, BRASIL');
