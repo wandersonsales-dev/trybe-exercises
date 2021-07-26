e0bf8f13e8892b7ae6b2ea44ef5b6c191f51e578
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-07-25 21:49:56 -0300

    :sparkles: Exercise 03
    3 - Utilizando uma query , encontre quantos caracteres temos em 'Uma frase qualquer'.

--- bloco_21/dia_1/exercises_manipulacao_de_strings.sql
+++ bloco_21/dia_1/exercises_manipulacao_de_strings.sql
@@ -1,10 +1,13 @@
 -- 1 - Faça uma query que exiba a palavra 'trybe' em CAIXA ALTA.
 SELECT
   UCASE('trybe');
--- 2 - Faça uma query que transforme a frase 'Você já ouviu falar do DuckDuckGo?' em 'Você já ouviu falar do Google?' .
+-- 2 - Faça uma query que transforme a frase 'Você já ouviu falar do DuckDuckGo?' em 'Você já ouviu falar do Google?'.
 SELECT
   REPLACE(
     'Você já ouviu falar do DuckDuckGo?',
     'DuckDuckGo',
     'Google'
   );
+-- 3 - Utilizando uma query , encontre quantos caracteres temos em 'Uma frase qualquer'.
+SELECT
+  LENGTH('Uma frase qualquer');
