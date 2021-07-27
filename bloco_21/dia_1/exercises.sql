c408980709d9571f674d85b83df5c84177fd37b6
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-07-26 21:47:38 -0300

    :sparkles: Exercise 06
    6. Escreva uma query que exiba a quantidade de pessoas que trabalham como pessoas programadoras ( IT_PROG ).

--- bloco_21/dia_1/exercises.sql
+++ bloco_21/dia_1/exercises.sql
@@ -31,3 +31,13 @@ SELECT
   ROUND(AVG(SALARY), 2)
 FROM
   hr.employees;
+-- 6. Escreva uma query que exiba a quantidade de pessoas que trabalham como pessoas programadoras ( IT_PROG ).
+SELECT
+  JOB_ID,
+  COUNT(*)
+FROM
+  hr.employees
+GROUP BY
+  JOB_ID
+HAVING
+  JOB_ID = 'IT_PROG';
