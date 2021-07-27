ee16096c48ee32e3ea231b2b2b3c4a7fcda51764
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-07-26 21:37:49 -0300

    :sparkles: Exercise 03
    3. Escreva uma query que exiba a média salarial de cada JOB_ID , ordenando pela média salarial em ordem decrescente.

--- bloco_21/dia_1/exercises.sql
+++ bloco_21/dia_1/exercises.sql
@@ -8,3 +8,13 @@ SELECT
   MAX(SALARY) - MIN(SALARY)
 FROM
   hr.employees;
+-- 3. Escreva uma query que exiba a média salarial de cada JOB_ID , ordenando pela média salarial em ordem decrescente.
+SELECT
+  JOB_ID,
+  AVG(SALARY) AS avg_salary
+FROM
+  hr.employees
+GROUP BY
+  JOB_ID
+ORDER BY
+  avg_salary DESC;
