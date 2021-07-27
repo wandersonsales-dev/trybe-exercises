1b90303f95777b407d1f53e481d2369753171c1d
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-07-26 21:44:01 -0300

    :sparkles: Exercise 05
    5. Escreva uma query que exiba quatro informações: o maior salário, o menor salário, a soma de todos os salários e a média dos salários. Todos os valores devem ser formatados para ter apenas duas casas decimais.

--- bloco_21/dia_1/exercises.sql
+++ bloco_21/dia_1/exercises.sql
@@ -23,3 +23,11 @@ SELECT
   SUM(SALARY)
 FROM
   hr.employees;
+-- 5. Escreva uma query que exiba quatro informações: o maior salário, o menor salário, a soma de todos os salários e a média dos salários. Todos os valores devem ser formatados para ter apenas duas casas decimais.
+SELECT
+  ROUND(MAX(SALARY), 2),
+  ROUND(MIN(SALARY), 2),
+  ROUND(SUM(SALARY), 2),
+  ROUND(AVG(SALARY), 2)
+FROM
+  hr.employees;
