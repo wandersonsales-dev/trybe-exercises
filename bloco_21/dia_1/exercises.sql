1f8fb162490c6bf72c1237dfa560365809c340fd
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-07-26 21:39:04 -0300

    :sparkles: Exercise 04
    4. Escreva uma query que exiba a quantidade de dinheiro necessária para realizar o pagamento de todas as pessoas funcionárias.

--- bloco_21/dia_1/exercises.sql
+++ bloco_21/dia_1/exercises.sql
@@ -18,3 +18,8 @@ GROUP BY
   JOB_ID
 ORDER BY
   avg_salary DESC;
+-- 4. Escreva uma query que exiba a quantidade de dinheiro necessária para realizar o pagamento de todas as pessoas funcionárias.
+SELECT
+  SUM(SALARY)
+FROM
+  hr.employees;
