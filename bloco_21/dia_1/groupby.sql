799eef90b6d398f3e39338c8ad5ed71b2258d5d4
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-07-26 21:07:00 -0300

    :sparkles: Exercise 03
    3 - Monte uma query que exiba a média de duração de locação por classificação indicativa ( rating ) dos filmes cadastrados na tabela sakila.film . Os resultados devem ser agrupados pela classificação indicativa e ordenados da maior média para a menor.

--- bloco_21/dia_1/groupby.sql
+++ bloco_21/dia_1/groupby.sql
@@ -16,3 +16,13 @@ FROM
 GROUP BY
   store_id,
   active;
+-- 3 - Monte uma query que exiba a média de duração de locação por classificação indicativa ( rating ) dos filmes cadastrados na tabela sakila.film . Os resultados devem ser agrupados pela classificação indicativa e ordenados da maior média para a menor.
+SELECT
+  rating,
+  AVG(rental_duration) AS media
+FROM
+  sakila.film
+GROUP BY
+  rating
+ORDER BY
+  media DESC;
