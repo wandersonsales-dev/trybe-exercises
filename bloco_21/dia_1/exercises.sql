3beb9aa763fae7fd8a09ad4a3fa428630bbaf594
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-07-26 21:34:52 -0300

    :sparkles: Exercise 02
    2. Escreva uma query que exiba a diferença entre o maior e o menor salário.

--- bloco_21/dia_1/exercises.sql
+++ bloco_21/dia_1/exercises.sql
@@ -3,3 +3,8 @@ SELECT
   MAX(SALARY)
 from
   hr.employees;
+-- 2. Escreva uma query que exiba a diferença entre o maior e o menor salário.
+SELECT
+  MAX(SALARY) - MIN(SALARY)
+FROM
+  hr.employees;
