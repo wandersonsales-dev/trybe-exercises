7718ef9ad05ae494ce1baf8885e94a901c14247b
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-07-26 21:22:33 -0300

    :sparkles: [Para Fixar] - Exercise 01
    1 - Usando a query a seguir, exiba apenas as durações médias que estão entre 115.0 a 121.50. Além disso, dê um alias (apelido) à coluna gerada por AVG(length) , de forma que deixe a query mais legível. Finalize ordenando os resultados de forma decrescente.

--- bloco_21/dia_1/groupby.sql
+++ bloco_21/dia_1/groupby.sql
@@ -36,3 +36,16 @@ GROUP BY
   district
 ORDER BY
   COUNT(*) DESC;
+-- 1 - Usando a query a seguir, exiba apenas as durações médias que estão entre 115.0 a 121.50. Além disso, dê um alias (apelido) à coluna gerada por AVG(length) , de forma que deixe a query mais legível. Finalize ordenando os resultados de forma decrescente.
+SELECT
+  rating,
+  AVG(length) AS 'media'
+FROM
+  sakila.film
+GROUP BY
+  rating
+HAVING
+  media BETWEEN 115.0
+  AND 121.50
+ORDER BY
+  media;
