f089f24b6ce9a06e3aef6172e2693f05b5c4cd86
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-07-26 21:24:30 -0300

    :sparkles: Exercise 02
    2 - Usando a query a seguir, exiba apenas os valores de custo de substituição que estão acima de $3950.50. Dê um alias que faça sentido para SUM(replacement_cost) , de forma que deixe a query mais legível. Finalize ordenando os resultados de forma crescente.

--- bloco_21/dia_1/groupby.sql
+++ bloco_21/dia_1/groupby.sql
@@ -49,3 +49,15 @@ HAVING
   AND 121.50
 ORDER BY
   media;
+-- 2 - Usando a query a seguir, exiba apenas os valores de custo de substituição que estão acima de $3950.50. Dê um alias que faça sentido para SUM(replacement_cost) , de forma que deixe a query mais legível. Finalize ordenando os resultados de forma crescente.
+SELECT
+  rating,
+  SUM(replacement_cost) total
+FROM
+  sakila.film
+GROUP by
+  rating
+HAVING
+  total > 3950.50
+ORDER BY
+  total;
